af361ceb735a36d98de4dd4d6aa57798528e8458
Brendon <Brendon4496@gmail.com>
2021-04-09 01:28:47 -0400

    + Added ReportingStructure and corresponding endpoint + Added testing for ReportingStructure

---
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -0,0 +1,86 @@
+package com.mindex.challenge.data;
+
+import com.mindex.challenge.dao.EmployeeRepository;
+
+import java.util.Map;
+
+public class ReportingStructure {
+    private EmployeeRepository employeeRepository;
+    private Employee employee;
+    private int numberOfReports;
+
+    public ReportingStructure(EmployeeRepository er, Employee e)
+    {
+        employeeRepository = er;
+        employee = e;
+        numberOfReports = getReportCount(0, e);
+    }
+
+    // Getters
+    public Employee getEmployee() { return this.employee; }
+    public int getNumberOfReports() { return this.numberOfReports; }
+
+    /**
+     * Recursively obtains the number of reports under a supplied employee.
+     * @param numberOfReports - The number of reports found at the time of recursion. Starts with a default value of 0.
+     * @param employee - The employee to find the number of reports under.
+     * @return The number of reports under a given employee.
+     */
+    private int getReportCount(int numberOfReports, Employee employee)
+    {
+        // I'm going to make this code active under the assumption that you cannot have an employee reporting under
+        // and above another employee simultaneously. But, just in case I misinterpreted and this ISN'T the case,
+        // I'll create another version of the function that checks for this.
+        // EX:
+        //          Kate                ...Dean
+        //          /  \                   /  \    <-- assuming this two-way is not possible
+        //       Pete   Dean...       Grace    Kate
+
+        if (employee.getDirectReports() == null)
+        {
+            return 1;
+        }
+        for (Employee e : employee.getDirectReports())
+        {
+            String id = e.getEmployeeId();
+            numberOfReports += getReportCount(numberOfReports, employeeRepository.findByEmployeeId(id));
+        }
+        return numberOfReports;
+    }
+
+    /**
+     * - CURRENTLY UNUSED - Overloaded Function -
+     * Recursively obtains the number of reports under a supplied employee, excluding duplicate entries.
+     * @param numberOfReports - The number of reports found at the time of recursion. Starts with a default value of 0.
+     * @param employee - The employee to find the number of reports under.
+     * @param dupeCheck - A map that lists all employees that have been found. INITIALIZE AS AN EMPTY MAP. Or,
+     *                  theoretically fix the function so it's overloaded, and the initial pass-in without the map
+     *                  will run this function. Only if duplicate entries is a problem in the first place.
+     * @return The number of reports under a given employee.
+     */
+    private int getReportCount(int numberOfReports, Employee employee, Map<String, Integer> dupeCheck)
+    {
+        //
+        //          Kate                ...Dean
+        //          /  \                   /  \    <-- assuming this two-way IS possible
+        //       Pete   Dean...       Grace    Kate
+        if (employee.getDirectReports() == null)
+        {
+            return 1;
+        }
+        for (Employee e : employee.getDirectReports())
+        {
+            String id = e.getEmployeeId();
+            if (!dupeCheck.containsKey(id)){
+                numberOfReports += getReportCount(numberOfReports, employeeRepository.findByEmployeeId(id));
+                dupeCheck.put(id, 1);
+            }
+            else {
+                // Currently, there's not really a purpose for adding to this number, but theoretically, if you wanted
+                // to check for how many times an employee is found in duplicate chains, you could use this.
+                dupeCheck.replace(id, dupeCheck.get(id) + 1);
+            }
+        }
+        return numberOfReports;
+    }
+}
--- src/test/java/com/mindex/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/mindex/challenge/ChallengeApplicationTests.java
@@ -1,16 +1,35 @@
 package com.mindex.challenge;
 
+import com.mindex.challenge.controller.EmployeeController;
+import com.mindex.challenge.data.ReportingStructure;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChallengeApplicationTests {
 
+	/**
+	 * I don't particularly feel like recreating tons of Employee objects, so I'll do the same Autowiring that the
+	 * DataBootstrapTest does to set up the rest of the tests.
+	 */
+	@Autowired
+	private EmployeeController employeeController;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void report_test() {
+		ReportingStructure rs = employeeController.report("16a596ae-edd3-4847-99fe-c4518e82c86f");
+		assertEquals(employeeController.read("16a596ae-edd3-4847-99fe-c4518e82c86f"), rs.getEmployee());
+		assertEquals(4, rs.getNumberOfReports());
+
+	}
 }
